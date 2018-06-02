346d252fdc92414f8db213ce955bb200e240b7a7
mzazakeith <mzazakeith@gmail.com>
2018-06-02 16:35:08 +0300

    added arguments for class user

--- src/app/users.ts
+++ src/app/users.ts
@@ -1,2 +1,6 @@
-export class Users {
+export class User {
+
+    constructor(public name: string, public login: string, public avatar_url: string, public html_url: string,
+                public public_repos: number, public created_at: Date, public followers: number, public following: number) {
+    }
 }
